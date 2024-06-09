cdc01162d910c7b842b367e83eafed4d2dbf6ac1
alexssandrodamiao <alexssandro.damiao@acertpix.com.br>
2024-06-09 17:46:54 -0300

    implementar while no codigo de kpi

--- kpi.py
+++ kpi.py
@@ -1,33 +1,38 @@
+nome_valido = False
+salario_valido = False
+bonus_valido = False
 
-nome = input('Digite seu nome: ')
-if nome.isdigit():
-    print('Voce nao digitou um texto valido')
-    exit() 
-elif nome.isspace():
-    print('Voce nao digitou um texto valido')
-    exit()
-elif len(nome) <= 3:
-    print('Voce nao digitou um texto valido')
-    exit()
-try:
-    salario = float(input('Digite o seu salário: '))
-except ValueError:
-    print('Valor digitado incorreto')
-    exit()
+while not nome_valido:
+    nome = input('Digite seu nome: ')
+    if nome.isdigit():
+        print('Voce nao digitou um texto valido')
+    elif nome.isspace():
+        print('Voce nao digitou um texto valido')
+    elif len(nome) <= 3:
+        print('Voce nao digitou um texto valido')
+    else:
+        nome_valido = True
 
-if salario < 1:
-    print('Salario incorreto')
-    exit()
+while not salario_valido:
+    try:
+        salario = float(input('Digite o seu salário: '))
+        if salario < 1:
+            print('Salario incorreto')
+        else:
+            salario_valido = True
+    except ValueError:
+        print('Valor digitado incorreto')
+
+while not bonus_valido:
     
-try:
-    valor_bonus = float(input('Digite o valor do bônus: '))
-except ValueError:
-    print('Valor digitado incorreto')
-    exit()
+    try:
+        valor_bonus = float(input('Digite o valor do bônus: '))
+        if valor_bonus == 0 :
+            print('valor bonus incorreto')
+        else:
+            bonus_valido = True
+    except ValueError:
+        print('Valor digitado incorreto')
 
-if valor_bonus == 0 :
-    print('valor bonus incorreto')
-    exit()
-    
 kpi = 1000 + salario * valor_bonus
 print(f'Olá {nome}, o seu valor bônus foi de {kpi}')
